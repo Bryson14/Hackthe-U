7149475d2a1ce3360ba149c11581b032ec944674
Peyton Kiel <uwdawg00@gmail.com>
2019-10-05 16:36:22 -0600

    add those buttons

--- HackTheU/src/mainMenu.java
+++ HackTheU/src/mainMenu.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -12,15 +14,39 @@ public class mainMenu extends Application {
 
         // create a button 
         Button rulesButton = new Button("Rules");
+        Button gameButton = new Button("Play Chess");
+        Button quitButton = new Button("Quit");
+
+        rulesButton.setTranslateX(0);
+        rulesButton.setTranslateY(10);
+        gameButton.setTranslateX(0);
+        gameButton.setTranslateY(-35);
+        quitButton.setTranslateX(0);
+        quitButton.setTranslateY(55);
+
+        EventHandler<ActionEvent> displayRules = e -> {
+
+        };
+
+        EventHandler<ActionEvent> startGame = e -> {
+
+        };
+        EventHandler<ActionEvent> quitGame = e -> {
+
+        };
+
+        rulesButton.setOnAction(displayRules);
+        gameButton.setOnAction(startGame);
+        quitButton.setOnAction(quitGame);
 
         // create a stack pane 
         StackPane r = new StackPane();
 
         // add button 
-        r.getChildren().add(rulesButton);
+        r.getChildren().addAll(rulesButton, gameButton, quitButton);
 
         // create a scene 
-        Scene sc = new Scene(r, 200, 200);
+        Scene sc = new Scene(r, 400, 400);
 
         // set the scene 
         s.setScene(sc);
