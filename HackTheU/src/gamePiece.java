c4f5e55484a529fd6a3c6d4f51b91a270ee12d5a
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-05 14:42:16 -0600

    team side and overloaded contrcutor

--- HackTheU/src/gamePiece.java
+++ HackTheU/src/gamePiece.java
@@ -2,5 +2,12 @@ public abstract class  gamePiece {
     private int posX;
     private int posY;
     private String name;
+    private boolean team;
 
+    public gamePiece(String name, int posX, int posY, boolean team) {
+        this.name = name;
+        this.posX = posX;
+        this.posY = posY;
+        this.team = team;
+    }
 }
