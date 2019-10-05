3da2cc5861a10859afbfd21d8e9cd61f53a73905
JustChels <chelsea.gunther@aggiemail.usu.edu>
2019-10-05 15:08:42 -0600

    the queen has entered the scene

---
+++ HackTheU/src/Queen.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Queen extends gamePiece {
+
+    Queen(String name, int posX, int posY, boolean team) {
+        super(name, posX, posY, team);
+        name = "Queen";
+
+    }
+
+    public void canMove() {
+
+    }
+
+    public void move(){
+
+    }
+
+    public ArrayList<String> moves() {
+
+    }
+}
