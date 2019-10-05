7910098f2b84cf2bdeb6b164845397325728e7b8
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-05 15:47:37 -0600

    mostly written except for logic

--- HackTheU/src/Bishop.java
+++ HackTheU/src/Bishop.java
@@ -1,2 +1,33 @@
-public class Bishop {
+import java.util.ArrayList;
+
+public class Bishop extends gamePiece{
+
+    Bishop(String name, int posX, int posY, boolean team) {
+        super(name, posX, posY, team);
+    }
+
+    @Override
+    public int getPosX() {
+        return super.getPosX();
+    }
+
+    @Override
+    public int getPosY() {
+        return super.getPosY();
+    }
+
+    @Override
+    public void setPosX(int posX) {
+        super.setPosX(posX);
+    }
+
+    @Override
+    public void setPosY(int posY) {
+        super.setPosY(posY);
+    }
+
+    @Override
+    public ArrayList<String> moves() {
+        return null;
+    }
 }
--- HackTheU/src/Pawn.java
+++ HackTheU/src/Pawn.java
@@ -1,2 +1,44 @@
-public class Pawn {
+import java.util.ArrayList;
+
+public class Pawn extends gamePiece{
+
+    private int numberOfMoves;
+
+    Pawn(String name, int posX, int posY, boolean team) {
+        super(name, posX, posY, team);
+        numberOfMoves = 0;
+    }
+
+    @Override
+    public int getPosY() {
+        return super.getPosY();
+    }
+
+    @Override
+    public int getPosX() {
+        return super.getPosX();
+    }
+
+    @Override
+    public void setPosY(int posY) {
+        super.setPosY(posY);
+    }
+
+    @Override
+    public void setPosX(int posX) {
+        super.setPosX(posX);
+    }
+
+    public int getNumberOfMoves() {
+        return numberOfMoves;
+    }
+
+    public void setNumberOfMoves(int numberOfMoves) {
+        this.numberOfMoves = numberOfMoves;
+    }
+
+    @Override
+    public ArrayList<String> moves() {
+        return null;
+    }
 }
