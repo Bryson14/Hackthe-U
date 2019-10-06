0afb9fa4576e4dd927322de3d68e1930ad372458
Rachael <rachael.fisher@aggiemail.usu.edu>
2019-10-05 21:51:48 -0600

    Rules

--- HackTheU/src/mainMenu.java
+++ HackTheU/src/mainMenu.java
@@ -64,34 +64,40 @@ public class mainMenu extends Application {
             pawn.setTranslateY(-150);
 
             Text rook = new Text("Rook = The rook piece can move forward, backward, left or right at any time. The rook piece can move anywhere from 1 to 7 squares \n" +
-                    "in any direction, \nso long as it is not obstructed by any other piece.");
+                    "in any direction, so long as it is not obstructed by any other piece.\n");
             rook.setTranslateX(0);
             rook.setTranslateY(-85);
 
             Text bishop = new Text("Bishop = The bishop can move in any direction diagonally, so long as it is not obstructed by another piece. The bishop piece \n" +
-                    "cannot move past any piece that is obstructing its path. The bishop can take any other piece on the board that is within its \n" +
-                    "bounds of movement.");
-            bishop.setTranslateX(0);
+                    "cannot move past any piece that is obstructing its path. The bishop can take any other piece on the board that is within its\n " +
+                    "bounds of movement.\n");
+            bishop.setTranslateX(-30);
             bishop.setTranslateY(-20);
 
-            Text knight = new Text("The Knight piece can move forward, backward, left or right two squares and \n" +
+            Text knight = new Text("The Knight = piece can move forward, backward, left or right two squares and \n" +
                     "must then move one square in either perpendicular direction. The Knight piece can only move \n" +
                     "to one of up to eight positions on the board. The Knight piece can move to any position not \n" +
                     "already inhabited by another piece of the same color. The Knight piece can skip over any \n" +
                     "other pieces to reach its destination position.\n");
+            knight.setTranslateX(-100);
+            knight.setTranslateY(60);
 
-            Text queen = new Text("The queen can move in any direction on a straight or diagonal path. The queen \n" +
+            Text queen = new Text("The queen = can move in any direction on a straight or diagonal path. The queen \n" +
                     "cannot \"jump\" over any piece on the board, so its movements are restricted to any \n" +
                     "direction of unoccupied squares. The queen can be used to capture any of your opponent's \n" +
                     "pieces on the board.");
+            queen.setTranslateX(-100);
+            queen.setTranslateY(135);
 
-            Text king = new Text("The king piece can move one single square in any direction. The king cannot \n" +
+            Text king = new Text("The king = piece can move one single square in any direction. The king cannot \n" +
                     "move onto a square that is currently occupied by a piece from its own team. The king piece \n" +
                     "cannot move to any square that puts them into a \"check\" position. The king piece can \n" +
                     "participate in a move known as \"castling\", where the piece can move up to three squares \n" +
                     "while exchanging places with a rook chess piece.");
+            king.setTranslateX(-110);
+            king.setTranslateY(225);
 
-            rulesPane.getChildren().addAll(title2, pawn, rook, bishop);
+            rulesPane.getChildren().addAll(title2, pawn, rook, bishop, knight, queen, king);
             Scene rulesScene = new Scene(rulesPane, 750,650);
             Stage rulesStage = new Stage();
             rulesStage.setScene(rulesScene);
