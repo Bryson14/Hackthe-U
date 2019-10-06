f379e8d8bdf5f07c883337968ab5033ba9486e6a
Peyton Kiel <uwdawg00@gmail.com>
2019-10-05 18:46:21 -0600

    fix

--- HackTheU/src/mainMenu.java
+++ HackTheU/src/mainMenu.java
@@ -3,7 +3,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -11,20 +10,18 @@ import javafx.stage.Stage;
 
 public class mainMenu extends Application {
 
-    public void start(Stage s)
-    {
-        // set title for the stage 
+    public void start(Stage s) {
+        openMainMenu(s);
+    }
+
+    private void openMainMenu(Stage s){
         s.setTitle("Chess");
 
         Text title = new Text("Chess");
         title.setTranslateX(0);
         title.setTranslateY(-110);
+        title.setFont(new Font("Algerian",70));
 
-        Font font = new Font("Algerian",70);
-
-        title.setFont(font);
-
-        // create a button
         Button gameButton = new Button("Play Chess");
         Button rulesButton = new Button("Rules");
         Button quitButton = new Button("Quit");
@@ -39,40 +36,34 @@ public class mainMenu extends Application {
 
         EventHandler<ActionEvent> startGame = e -> {
             StackPane root2 = new StackPane();
-//            Label label = new Label("Your are now in the second form");
-//            root2.getChildren().add(label);
-            Scene secondScene = new Scene(root2, 500,500);
+            Scene secondScene = new Scene(root2, 750,650);
             Stage secondStage = new Stage();
-            secondStage.setScene(secondScene); // set the scene
+            secondStage.setScene(secondScene);
             secondStage.setTitle("Second Form");
-            chessBoardGUI.start2(secondStage);
-//            secondStage.show();
+//            chessBoardGUI.start2(secondStage);
+            secondStage.show();
             s.close();
         };
 
         EventHandler<ActionEvent> displayRules = e -> {
 
-        };
-
-
-        EventHandler<ActionEvent> quitGame = e -> {
-            s.close();
 
         };
+
+
+        EventHandler<ActionEvent> quitGame = e -> s.close();
 
         gameButton.setOnAction(startGame);
         rulesButton.setOnAction(displayRules);
         quitButton.setOnAction(quitGame);
 
-        // create a stack pane 
         StackPane buttonHolder = new StackPane();
 
-        // add button 
         buttonHolder.getChildren().addAll(gameButton, rulesButton, quitButton, title);
         buttonHolder.setStyle("-fx-background-color: rgba(255,186,26,0.64)");
 
         // create a scene 
-        Scene sc = new Scene(buttonHolder, 600, 600);
+        Scene sc = new Scene(buttonHolder, 750, 650);
 
         // set the scene 
         s.setScene(sc);
