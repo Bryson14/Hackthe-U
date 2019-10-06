f6a164ab77bf04cfcd6039c59425353fdff90380
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-10-06 00:58:27 -0600

    removed stupid functions dude

--- HackTheU/src/gamePiece.java
+++ HackTheU/src/gamePiece.java
@@ -48,29 +48,6 @@ public abstract class  gamePiece {
         return numberOfMoves;
     }
 
-    ArrayList<Coordinates> whatsInTheWay(gamePiece[][] grid, ArrayList<Coordinates> moves) {
-
-        // removes moves outside of board
-        int i = 0;
-        while (i < moves.size()){
-            if (moves.get(i).x > 7 || moves.get(i).x < 0 || moves.get(i).y > 7 || moves.get(i).y < 0) { //outside board
-                moves.remove(i);
-            } else i++; // moves increment forward if nothing was removed
-        }
-
-         //check to see if something is in the way
-         int j = 0;
-        while (j < moves.size()) {
-            if (grid[moves.get(j).x][moves.get(j).y] != null) {
-                if (getTeam() == team)
-                moves.remove(j);
-                j++;
-            }
-
-        }
-        return moves;
-    }
-
     boolean isEnemy(gamePiece[][] grid, gamePiece piece) {
         if (piece.getTeam() == getTeam()) return false;
         else return true;
