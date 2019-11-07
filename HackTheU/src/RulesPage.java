f09a02c4d5d554182673a9f3e5cbc2adf2ce12dc
Peyton Kiel <uwdawg00@gmail.com>
2019-11-07 11:43:45 -0700

    Fix RulePage

--- HackTheU/src/RulesPage.java
+++ HackTheU/src/RulesPage.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 public class RulesPage extends mainMenu{
     public static void rulesPage(Stage stage) {
+        int wrapText = 600;
         Pane rulesPane = new Pane();
         rulesPane.setStyle("-fx-background-color: rgba(255,186,26,0.64)");
 
@@ -20,39 +21,50 @@ public class RulesPage extends mainMenu{
             stage.close();
         });
         javafx.scene.text.Font ruleFont = new Font("Comic Sans", 12);
-        Text pawn = new Text("Pawn = Pawn chess pieces can only directly forward one square, with two exceptions.\n" +
-                "Pawns can move directly forward two squares on their first move only. Pawns can move diagonally \n" +
-                "forward when capturing an opponent's chess piece. Once a pawn chess piece reaches the other side \n" +
-                " of the chess board, the player may \"trade\" the pawn in for any other chess piece if they \n" +
+        Text pawn = new Text("THE PAWN\nPawn chess pieces can only directly forward one square, with two exceptions. " +
+                "Pawns can move directly forward two squares on their first move only. Pawns can move diagonally " +
+                "forward when capturing an opponent's chess piece. Once a pawn chess piece reaches the other side " +
+                " of the chess board, the player may \"trade\" the pawn in for any other chess piece if they " +
                 "choose, except another king.\n");
         pawn.setFont(ruleFont);
+        pawn.setWrappingWidth(wrapText);
 
-        Text rook = new Text("Rook = The rook piece can move forward, backward, left or right at any time. The \n" +
-                "rook piece can move anywhere from 1 to 7 squares in any direction, so long as it is not \n" +
+        Text rook = new Text("THE ROOK\nThe rook piece can move forward, backward, left or right at any time. The " +
+                "rook piece can move anywhere from 1 to 7 squares in any direction, so long as it is not " +
                 "obstructed by any other piece.\n");
+        rook.setFont(ruleFont);
+        rook.setWrappingWidth(wrapText);
 
-        Text bishop = new Text("Bishop = The bishop can move in any direction diagonally, so long as it is not \n" +
-                "obstructed by another piece. The bishop piece cannot move past any piece that is obstructing \n" +
-                "its path. The bishop can take any other piece on the board that is within its bounds of movement.");
+        Text bishop = new Text("THE BISHOP\nThe bishop can move in any direction diagonally, so long as it is not " +
+                "obstructed by another piece. The bishop piece cannot move past any piece that is obstructing " +
+                "its path. The bishop can take any other piece on the board that is within its bounds of movement.\n");
+        bishop.setFont(ruleFont);
+        bishop.setWrappingWidth(wrapText);
 
-        Text knight = new Text("The Knight = piece can move forward, backward, left or right two squares and \n" +
-                "must then move one square in either perpendicular direction. The Knight piece can only move \n" +
-                "to one of up to eight positions on the board. The Knight piece can move to any position not \n" +
-                "already inhabited by another piece of the same color. The Knight piece can skip over any \n" +
+        Text knight = new Text("THE KNIGHT\nThis piece can move forward, backward, left or right two squares and " +
+                "must then move one square in either perpendicular direction. The Knight piece can only move " +
+                "to one of up to eight positions on the board. The Knight piece can move to any position not " +
+                "already inhabited by another piece of the same color. The Knight piece can skip over any " +
                 "other pieces to reach its destination position.\n");
+        knight.setFont(ruleFont);
+        knight.setWrappingWidth(wrapText);
 
-        Text queen = new Text("The queen = can move in any direction on a straight or diagonal path. The queen \n" +
-                "cannot \"jump\" over any piece on the board, so its movements are restricted to any \n" +
-                "direction of unoccupied squares. The queen can be used to capture any of your opponent's \n" +
+        Text queen = new Text("THE QUEEN\nThe queen can move in any direction on a straight or diagonal path. The queen " +
+                "cannot \"jump\" over any piece on the board, so its movements are restricted to any " +
+                "direction of unoccupied squares. The queen can be used to capture any of your opponent's " +
                 "pieces on the board.\n");
+        queen.setFont(ruleFont);
+        queen.setWrappingWidth(wrapText);
 
-        Text king = new Text("The king = piece can move one single square in any direction. The king cannot \n" +
-                "move onto a square that is currently occupied by a piece from its own team. The king piece \n" +
-                "cannot move to any square that puts them into a \"check\" position. The king piece can \n" +
-                "participate in a move known as \"castling\", where the piece can move up to three squares \n" +
+        Text king = new Text("THE KING\nThis piece can move one single square in any direction. The king cannot " +
+                "move onto a square that is currently occupied by a piece from its own team. The king piece " +
+                "cannot move to any square that puts them into a \"check\" position. The king piece can " +
+                "participate in a move known as \"castling\", where the piece can move up to three squares " +
                 "while exchanging places with a rook chess piece.\n");
+        king.setFont(ruleFont);
+        king.setWrappingWidth(wrapText);
 
-        VBox vBox = new VBox(title2, king, queen, knight, bishop, rook, pawn, respawn);
+        VBox vBox = new VBox(title2, pawn, rook, bishop, knight, queen, king, respawn);
         BorderPane bp = new BorderPane();
         bp.setCenter(vBox);
         vBox.setAlignment(Pos.CENTER);
--- HackTheU/src/chessBoardGUI.java
+++ HackTheU/src/chessBoardGUI.java
@@ -92,50 +92,14 @@ public class chessBoardGUI extends  mainMenu {
                 }
             }
         }
-//
-//
-//                final String[] imgHolder = {""};
-//                final Coordinates[] savedcoor = {};
-//                button.setOnAction(event -> {
-//                    //first click
-//                    Coordinates coor = new Coordinates((int)(button.getId().toCharArray())[0], (int)(button.getId().toCharArray())[1]);
-//                    if (movesList.isEmpty()) {
-//
-//                        //user clicked on the right team whose turn it is
-//                        if (cb.isOccupiedWithCorrectTeam(coor)) {
-//                            movesList.retainAll(cb.getAvailableMoves(coor));
-//                        }
-//                        else { //user clicked on an empty spot or on the team whose turn it is not
-//                            //do nothing
-//                        }
-//
-////                        String number = icons.get(Integer.parseInt(button.getId()) - 1);
-////                        movesList.add(number);
-//                        savedcoor[0] = coor;
-//                        imgHolder[0] = map.get(coor);
-//                        ImageView imageView3 = new ImageView(new Image(chessBoardGUI.class.getResourceAsStream(" ")));
-//                        button.setGraphic(imageView3);
-//                    }
-//                    // second click
-//                    else{
-//                        if (movesList.contains(coor)) {
-//                            cb.movePiece(savedcoor[0], coor);
-//                            //this is then the destination spot
-//
-//
-//                        } else {
-//                            //do nothing
-//                        }
-//                        movesList.clear(); // setting up for the first click again
-//
-//                        ImageView imageView2 = new ImageView(new Image(chessBoardGUI.class.getResourceAsStream(imgHolder[0])));
-//                        imageView2.setFitWidth(40);
-//                        imageView2.setFitHeight(40);
-//                        button.setGraphic(imageView2);
-//                        movesList.remove(0);
-//                    }
-//                });
-
+                Button main = new Button("Main Menu");
+        main.setOnAction(event -> {
+            mainMenu.openMainMenu(new Stage());
+            stage.close();
+        });
+                layout.getChildren().add(main);
+                main.setTranslateX(0);
+                main.setTranslateY(300);
                 Scene scene = new Scene(layout, 750, 650);
                 stage.setTitle("Chess Board");
                 stage.setScene(scene);
