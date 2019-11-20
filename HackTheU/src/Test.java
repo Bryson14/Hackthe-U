5bb6ded87bac2445df2d0b1a8a00d684c0f28761
JustChels <chelsea.gunther@aggiemail.usu.edu>
2019-11-19 17:23:46 -0700

    cleaning crap up

--- HackTheU/src/Test.java
+++ HackTheU/src/Test.java
@@ -1,47 +1,51 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.media.*;
 import javafx.scene.media.MediaPlayer;
 import java.io.File;
 
-// Sorry Bryson, I was tweaking this to see if this drag thing would work.
 
-public class anotherTest extends Application {
+public class Test extends Application {
     static class Delta { double x, y; }
 
     @Override
     public void start(Stage primaryStage) {
+        // Create instance of Chess game
+        Chess chess = new Chess();
+
+        // Drop down menu
+        Button Avengers = new Button("AVENGERS");
+        Button Normal = new Button("NORMAL");
+
+        final ComboBox menu = new ComboBox();
+        menu.getItems().addAll(
+                Avengers, Normal);
+        menu.setPromptText("Settings");
+
+        // Add sounds if buttons are clicked on
         String sep = System.getProperty("file.separator") + System.getProperty("file.separator");
         String srcDir = System.getProperty("user.dir") + sep + "HackTheU" + sep + "src" + sep;
-        File file = new File(srcDir + "sounds" + sep +"assemble.mp3");
-        Chess chess = new Chess();
-        chess.updateText("AVENGERS ASSEMBLE");
-        Button bt = new Button("AVENGERS");
-//        String assemble = ("file");
+        File file = new File(srcDir + "sounds" + sep + "assemble.mp3");
         Media sound = new Media(file.toURI().toString());
         MediaPlayer player = new MediaPlayer(sound);
-        bt.setOnAction(e->{
+        Avengers.setOnAction(e->{
             chess.changeStyle("avengers");
             player.play();
-//            player.setVolume(0.5);
         });
-        String sep1 = System.getProperty("file.separator") + System.getProperty("file.separator");
-        String srcDir1 = System.getProperty("user.dir") + sep1 + "HackTheU" + sep1 + "src" + sep1;
-        File file1 = new File(srcDir1 + "sounds" + sep1 +"short-definite-fart.wav");
-        Button bt2 = new Button("NORMAL");
-//        String fart = "/src/sounds\\short-definite-fart.wav";
+        String srcDir1 = System.getProperty("user.dir") + sep + "HackTheU" + sep + "src" + sep;
+        File file1 = new File(srcDir1 + "sounds" + sep +"short-definite-fart.wav");
         Media sound1 = new Media((file1).toURI().toString());
         MediaPlayer player1 = new MediaPlayer(sound1);
-        bt2.setOnAction(e->{
+        Normal.setOnAction(e->{
             chess.changeStyle("normal");
             player1.play();
         });
-        primaryStage.setScene(new Scene(new VBox(chess, new HBox(bt, bt2))));
 
+        primaryStage.setScene(new Scene(new VBox(menu, chess)));
         primaryStage.show();
 }
 }
