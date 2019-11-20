d7bac4d821009ebe7519e109d16747fe4e855435
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-11-19 17:26:48 -0700

    havent tried them yet

--- HackTheU/src/ChessServer.java
+++ HackTheU/src/ChessServer.java
@@ -65,7 +65,7 @@ public class ChessServer extends Pane{
         writer.flush();
     }
 
-    private void recieveMove() throws IOException{
+    private void receiveMove() throws IOException{
         String from = reader.readLine().trim();
         String to = reader.readLine().trim();
         cb.movePiece(new Coordinates(from), new Coordinates(to));
@@ -119,7 +119,7 @@ public class ChessServer extends Pane{
                                 cb.movePiece(lastCoor, coor);
                                 updateBoard(lastCoor, coor);
                                 displayTurn();
-                                recieveMove();
+                                receiveMove();
                             } catch (IOException ex) {
                                 System.out.println("There was a problem sending the move (Server)" + ex.toString());
                             }
@@ -348,7 +348,7 @@ public class ChessServer extends Pane{
         updateText("hello");
         try {
             connect();
-            recieveMove();
+            receiveMove();
         } catch (IOException e) {
             System.out.println("There was a problem with the server io" + e.toString());
         }
