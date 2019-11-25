0717fa19a10a1f28b7611c145682c44362a25bb7
Peyton Kiel <uwdawg00@gmail.com>
2019-11-25 16:13:29 -0700

    rules ocd

--- HackTheU/src/RulesPage.java
+++ HackTheU/src/RulesPage.java
@@ -31,46 +31,45 @@ public class RulesPage extends mainMenu{
         // Test parameters and Text
         int wrapText = 600;
         javafx.scene.text.Font ruleFont = new Font("Comic Sans", 12);
-        Text pawn = new Text("THE PAWN\nPawn chess pieces can only directly forward one square, with two exceptions. " +
-                "Pawns can move directly forward two squares on their first move only. Pawns can move diagonally " +
-                "forward when capturing an opponent's chess piece. Once a pawn chess piece reaches the other side " +
-                " of the chess board, the player may \"trade\" the pawn in for any other chess piece if they " +
-                "choose, except another king.\n");
+        Text pawn = new Text("THE PAWN\nPawn chess pieces can only directly forward one square, with two exceptions." +
+                " Pawns can move directly forward two squares on their first move only. Pawns can move diagonally fo" +
+                "rward when capturing an opponent's chess piece. Once a pawn chess piece reaches the other side of t" +
+                "he chess board, the player may \"trade\" the pawn in for any other chess piece if they choose, exce" +
+                "pt another king.\n");
         pawn.setFont(ruleFont);
         pawn.setWrappingWidth(wrapText);
 
-        Text rook = new Text("THE ROOK\nThe rook piece can move forward, backward, left or right at any time. The " +
-                "rook piece can move anywhere from 1 to 7 squares in any direction, so long as it is not " +
-                "obstructed by any other piece.\n");
+        Text rook = new Text("THE ROOK\nThe rook piece can move forward, backward, left or right at any time. The ro" +
+                "ok piece can move anywhere from 1 to 7 squares in any direction, so long as it is not obstructed by" +
+                " any other piece.\n");
         rook.setFont(ruleFont);
         rook.setWrappingWidth(wrapText);
 
-        Text bishop = new Text("THE BISHOP\nThe bishop can move in any direction diagonally, so long as it is not " +
-                "obstructed by another piece. The bishop piece cannot move past any piece that is obstructing " +
-                "its path. The bishop can take any other piece on the board that is within its bounds of movement.\n");
+        Text bishop = new Text("THE BISHOP\nThe bishop can move in any direction diagonally, so long as it is not ob" +
+                "structed by another piece. The bishop piece cannot move past any piece that is obstructing its path" +
+                ". The bishop can take any other piece on the board that is within its bounds of movement.\n");
         bishop.setFont(ruleFont);
         bishop.setWrappingWidth(wrapText);
 
-        Text knight = new Text("THE KNIGHT\nThis piece can move forward, backward, left or right two squares and " +
-                "must then move one square in either perpendicular direction. The Knight piece can only move " +
-                "to one of up to eight positions on the board. The Knight piece can move to any position not " +
-                "already inhabited by another piece of the same color. The Knight piece can skip over any " +
-                "other pieces to reach its destination position.\n");
+        Text knight = new Text("THE KNIGHT\nThis piece can move forward, backward, left or right two squares and mus" +
+                "t then move one square in either perpendicular direction. The Knight piece can only move to one of " +
+                "up to eight positions on the board. The Knight piece can move to any position not already inhabited" +
+                " by another piece of the same color. The Knight piece can skip over any other pieces to reach its d" +
+                "estination position.\n");
         knight.setFont(ruleFont);
         knight.setWrappingWidth(wrapText);
 
-        Text queen = new Text("THE QUEEN\nThe queen can move in any direction on a straight or diagonal path. The queen " +
-                "cannot \"jump\" over any piece on the board, so its movements are restricted to any " +
-                "direction of unoccupied squares. The queen can be used to capture any of your opponent's " +
-                "pieces on the board.\n");
+        Text queen = new Text("THE QUEEN\nThe queen can move in any direction on a straight or diagonal path. The qu" +
+                "een cannot \"jump\" over any piece on the board, so its movements are restricted to any direction o" +
+                "f unoccupied squares. The queen can be used to capture any of your opponent's pieces on the board.\n");
         queen.setFont(ruleFont);
         queen.setWrappingWidth(wrapText);
 
-        Text king = new Text("THE KING\nThis piece can move one single square in any direction. The king cannot " +
-                "move onto a square that is currently occupied by a piece from its own team. The king piece " +
-                "cannot move to any square that puts them into a \"check\" position. The king piece can " +
-                "participate in a move known as \"castling\", where the piece can move up to three squares " +
-                "while exchanging places with a rook chess piece.\n");
+        Text king = new Text("THE KING\nThis piece can move one single square in any direction. The king cannot move" +
+                " onto a square that is currently occupied by a piece from its own team. The king piece cannot move " +
+                "to any square that puts them into a \"check\" position. The king piece can participate in a move kn" +
+                "own as \"castling\", where the piece can move up to three squares while exchanging places with a ro" +
+                "ok chess piece.\n");
         king.setFont(ruleFont);
         king.setWrappingWidth(wrapText);
 
